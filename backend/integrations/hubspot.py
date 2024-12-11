d5e88e513ce7222a7767458efc981c07b1442d38
Kaushal_26 <kaushalsolanki260102@gmail.com>
2024-12-11 23:19:50 +0530

    feat: Add HubSpot Load Items using contacts of a company

--- backend/integrations/hubspot.py
+++ backend/integrations/hubspot.py
@@ -11,7 +11,9 @@ from dotenv import load_dotenv
 from fastapi.responses import HTMLResponse
 import httpx
 
+from integrations.integration_item import IntegrationItem
 from redis_client import add_key_value_redis, delete_key_redis, get_value_redis
+from hubspot import HubSpot
 
 load_dotenv()
 
@@ -19,7 +21,7 @@ CLIENT_ID = os.getenv("HUBSPOT_CLIENT_ID")
 CLIENT_SECRET = os.getenv("HUBSPOT_CLIENT_SECRET")
 
 REDIRECT_URI = "http://localhost:8000/integrations/hubspot/oauth2callback"
-SCOPES = "content"
+SCOPES = "crm.objects.contacts.read crm.objects.companies.read"
 authorization_url = f"https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
 
 
@@ -108,10 +110,72 @@ async def get_hubspot_credentials(user_id, org_id):
 
     return credentials
 
-async def create_integration_item_metadata_object(response_json):
-    # TODO
-    pass
+async def fetch_contacts_of_companies(api_client: HubSpot):
+    """
+    Fetch the contacts of the companies
+    """
+    companies_with_contacts = api_client.crm.companies.get_all(associations=["contacts"])
+    companies_with_contacts = [company.to_dict() for company in companies_with_contacts]
+    return companies_with_contacts
 
-async def get_items_hubspot(credentials):
-    # TODO
-    pass
+async def fetch_item_as_hubspot_contact(api_client: HubSpot, contact_id: str):
+    """
+    Fetch the contact of the company
+    """
+    contact = api_client.crm.contacts.basic_api.get_by_id(contact_id)
+    return contact.to_dict()
+
+async def create_integration_item_metadata_object(contact: dict, company: dict) -> IntegrationItem:
+    """
+    Create the integration item metadata object
+    """
+    response = IntegrationItem(
+        id=contact.get("id"),
+        type="hubspot_contacts_of_company",
+        name=f"{contact.get('properties').get('firstname')} {contact.get('properties').get('lastname')}",
+        parent_id=company.get("id"),
+        parent_path_or_name=company.get("properties").get("name"),
+        creation_time=contact.get("created_at"),
+        last_modified_time=contact.get("updated_at"),
+        visibility=not contact.get("archived"),
+    )
+    return response
+
+async def get_items_hubspot(credentials: str):
+    """
+    Get the items from HubSpot
+    - Here we are fetching the contacts of the companies as integration items.
+    """
+
+    credentials = json.loads(credentials)
+    api_client = HubSpot(access_token=credentials.get("access_token"))
+
+    list_of_integration_item_metadata = []
+
+    companies_with_contacts = await fetch_contacts_of_companies(api_client)
+    for company in companies_with_contacts:
+        contact_ids = [
+            contact.get("id") for contact in company.get("associations").get("contacts").get("results")
+            if contact.get("type") == "company_to_contact"
+        ]
+
+        # Make a batch async API call to fetch the contacts
+        tasks_fetch_contacts = [
+            fetch_item_as_hubspot_contact(api_client, contact_id)
+            for contact_id in contact_ids
+        ]
+        fetched_contacts = await asyncio.gather(*tasks_fetch_contacts)
+
+        # Make integration item metadata objects asynchronously
+        tasks_make_integration_item_metadata = [
+            create_integration_item_metadata_object(contact, company)
+            for contact in fetched_contacts
+        ]
+        new_integration_item_metadata = await asyncio.gather(*tasks_make_integration_item_metadata)
+
+        list_of_integration_item_metadata.extend(new_integration_item_metadata)
+
+    print(list_of_integration_item_metadata)
+
+    return list_of_integration_item_metadata
+
