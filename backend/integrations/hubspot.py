628865fbbeb0faa8378a860f9bf658f284b7da5e
Kaushal_26 <kaushalsolanki260102@gmail.com>
2024-12-11 19:18:08 +0530

    feat: Add HubSpot OAuth Integration

--- backend/integrations/hubspot.py
+++ backend/integrations/hubspot.py
@@ -1,18 +1,112 @@
 # slack.py
 
-from fastapi import Request
+import asyncio
+import base64
+import json
+import os
+import secrets
+from fastapi import HTTPException, Request
 
-async def authorize_hubspot(user_id, org_id):
-    # TODO
-    pass
+from dotenv import load_dotenv
+from fastapi.responses import HTMLResponse
+import httpx
+
+from redis_client import add_key_value_redis, delete_key_redis, get_value_redis
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("HUBSPOT_CLIENT_ID")
+CLIENT_SECRET = os.getenv("HUBSPOT_CLIENT_SECRET")
+
+REDIRECT_URI = "http://localhost:8000/integrations/hubspot/oauth2callback"
+SCOPES = "content"
+authorization_url = f"https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
+
+
+async def authorize_hubspot(user_id: str, org_id: str):
+    """
+    Authorize the user to HubSpot
+    """
+    state_data = {
+        "state": secrets.token_urlsafe(32),
+        "user_id": user_id,
+        "org_id": org_id,
+    }
+    encoded_state = base64.urlsafe_b64encode(json.dumps(state_data).encode("utf-8")).decode("utf-8")
+
+    auth_url = f"{authorization_url}&state={encoded_state}"
+    await add_key_value_redis(f"hubspot_state:{org_id}:{user_id}", json.dumps(state_data), expire=600)
+
+    return auth_url
+
 
 async def oauth2callback_hubspot(request: Request):
-    # TODO
-    pass
+    """
+    Handle the OAuth2 callback for HubSpot
+    """
+    if request.query_params.get("error"):
+        raise HTTPException(status_code=400, detail=request.query_params.get("error_description"))
+
+    code = request.query_params.get("code")
+    encoded_state = request.query_params.get("state")
+    state_data = json.loads(base64.urlsafe_b64decode(encoded_state).decode("utf-8"))
+    
+    original_state = state_data.get("state")
+    user_id = state_data.get("user_id")
+    org_id = state_data.get("org_id")
+
+    saved_state = await get_value_redis(f"hubspot_state:{org_id}:{user_id}")
+
+    if not saved_state or original_state != json.loads(saved_state).get("state"):
+        raise HTTPException(status_code=400, detail="State does not match.")
+
+    # Get the access token
+    async with httpx.AsyncClient() as client:
+        response, _ = await asyncio.gather(
+            client.post(
+                "https://api.hubapi.com/oauth/v1/token",
+                data={
+                    "grant_type": "authorization_code",
+                    "client_id": CLIENT_ID,
+                    "client_secret": CLIENT_SECRET,
+                    "redirect_uri": REDIRECT_URI,
+                    "code": code,
+                },
+                headers={
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            ),
+            delete_key_redis(f'hubspot_state:{org_id}:{user_id}'),
+        )
+        response.raise_for_status()
+    
+    await add_key_value_redis(f"hubspot_credentials:{org_id}:{user_id}", json.dumps(response.json()), expire=600)
+
+    close_window_script = """
+    <html>
+        <script>
+            window.close();
+        </script>
+    </html>
+    """
+
+    return HTMLResponse(content=close_window_script)
 
 async def get_hubspot_credentials(user_id, org_id):
-    # TODO
-    pass
+    """
+    Get the HubSpot credentials for the user
+    """
+    credentials = await get_value_redis(f"hubspot_credentials:{org_id}:{user_id}")
+    if not credentials:
+        raise HTTPException(status_code=400, detail="No credentials found for HubSpot integration.")
+    
+    credentials = json.loads(credentials)
+    if not credentials:
+        raise HTTPException(status_code=400, detail="No credentials found for HubSpot integration.")
+    
+    await delete_key_redis(f"hubspot_credentials:{org_id}:{user_id}")
+
+    return credentials
 
 async def create_integration_item_metadata_object(response_json):
     # TODO
