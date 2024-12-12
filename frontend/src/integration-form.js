cd38c9b63d477fc931bab16ed171d562d95fc6c3
Kaushal_26 <kaushalsolanki260102@gmail.com>
2024-12-12 23:14:05 +0530

    style: indent using prettier for js files

--- frontend/src/integration-form.js
+++ frontend/src/integration-form.js
@@ -1,73 +1,89 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
-    Box,
-    Autocomplete,
-    TextField,
-    Typography,
-    Divider,
-} from '@mui/material';
-import { AirtableIntegration } from './integrations/airtable';
-import { NotionIntegration } from './integrations/notion';
-import { HubspotIntegration } from './integrations/hubspot';
-import { DataForm } from './data-form';
+  Box,
+  Autocomplete,
+  TextField,
+  Typography,
+  Divider,
+} from "@mui/material";
+import { AirtableIntegration } from "./integrations/airtable";
+import { NotionIntegration } from "./integrations/notion";
+import { HubspotIntegration } from "./integrations/hubspot";
+import { DataForm } from "./data-form";
 
 const integrationMapping = {
-    'Notion': NotionIntegration,
-    'Airtable': AirtableIntegration,
-    'HubSpot': HubspotIntegration,
+  Notion: NotionIntegration,
+  Airtable: AirtableIntegration,
+  HubSpot: HubspotIntegration,
 };
 
 export const IntegrationForm = () => {
-    const [integrationParams, setIntegrationParams] = useState({});
-    const [user, setUser] = useState('TestUser');
-    const [org, setOrg] = useState('TestOrg');
-    const [currType, setCurrType] = useState(null);
-    const CurrIntegration = integrationMapping[currType];
+  const [integrationParams, setIntegrationParams] = useState({});
+  const [user, setUser] = useState("TestUser");
+  const [org, setOrg] = useState("TestOrg");
+  const [currType, setCurrType] = useState(null);
+  const CurrIntegration = integrationMapping[currType];
 
-    const handleIntegrationTypeChange = (e, value) => {
-        // Reset integrationParams when integration type changes
-        setIntegrationParams({});
-        setCurrType(value);
-    };
+  const handleIntegrationTypeChange = (e, value) => {
+    // Reset integrationParams when integration type changes
+    setIntegrationParams({});
+    setCurrType(value);
+  };
 
   return (
-    <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column' sx={{ width: '100%' }}>
-        <Typography variant="h4" gutterBottom sx={{mt: 4}}>
-            Integration Form
-            <Divider sx={{width: '100%', mt: 2}} />
-        </Typography>
-        <Box display='flex' flexDirection='column'>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      sx={{ width: "100%" }}
+    >
+      <Typography variant="h4" gutterBottom sx={{ mt: 4 }}>
+        Integration Form
+        <Divider sx={{ width: "100%", mt: 2 }} />
+      </Typography>
+      <Box display="flex" flexDirection="column">
         <TextField
-            label="User"
-            value={user}
-            onChange={(e) => setUser(e.target.value)}
-            sx={{mt: 2}}
+          label="User"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          sx={{ mt: 2 }}
         />
         <TextField
-            label="Organization"
-            value={org}
-            onChange={(e) => setOrg(e.target.value)}
-            sx={{mt: 2}}
+          label="Organization"
+          value={org}
+          onChange={(e) => setOrg(e.target.value)}
+          sx={{ mt: 2 }}
         />
         <Autocomplete
-            id="integration-type"
-            options={Object.keys(integrationMapping)}
-            sx={{ width: 300, mt: 2 }}
-            renderInput={(params) => <TextField {...params} label="Integration Type" />}
-            onChange={handleIntegrationTypeChange}
-            value={currType}
+          id="integration-type"
+          options={Object.keys(integrationMapping)}
+          sx={{ width: 300, mt: 2 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Integration Type" />
+          )}
+          onChange={handleIntegrationTypeChange}
+          value={currType}
         />
-        </Box>
-        {currType &&
+      </Box>
+      {currType && (
         <Box>
-            <CurrIntegration user={user} org={org} integrationParams={integrationParams} setIntegrationParams={setIntegrationParams} />
+          <CurrIntegration
+            user={user}
+            org={org}
+            integrationParams={integrationParams}
+            setIntegrationParams={setIntegrationParams}
+          />
         </Box>
-        }
-        {integrationParams?.credentials &&
-        <Box sx={{mt: 2}}>
-            <DataForm integrationType={integrationParams?.type} credentials={integrationParams?.credentials} />
+      )}
+      {integrationParams?.credentials && (
+        <Box sx={{ mt: 2 }}>
+          <DataForm
+            integrationType={integrationParams?.type}
+            credentials={integrationParams?.credentials}
+          />
         </Box>
-        }
+      )}
     </Box>
   );
-}
+};
